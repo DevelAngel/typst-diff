cef5413cec80c4224e5beba56fa1049467e6b01d
Angelos Drossos <develangel@mail.drossos.de>
2024-02-29 22:29:58 +0100

    test: add new equal test
    Test the stability of the Space element handling for three lines.

--- crates/typst-diff-cli/tests/equal-texts.rs
+++ crates/typst-diff-cli/tests/equal-texts.rs
@@ -12,3 +12,30 @@ fn one_vs_two_lines() {
         )
     );
 }
+
+#[test]
+fn one_vs_three_lines() {
+    test_diff!(
+        formatdoc!(r#"Hello World! I am here. How are you?"#),
+        formatdoc!(
+            r#"Hello World!
+               I am here.
+               How are you?"#
+        )
+    );
+}
+
+#[test]
+fn two_vs_three_lines() {
+    test_diff!(
+        formatdoc!(
+            r#"Hello World!
+               I am here. How are you?"#
+        ),
+        formatdoc!(
+            r#"Hello World!
+               I am here.
+               How are you?"#
+        )
+    );
+}
